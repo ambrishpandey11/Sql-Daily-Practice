08225343017a79921c0866a78ebeb15f43baa218
Ambrish Pandey <ambrishpandey.work@gmail.com>
2026-03-15 04:18:47 +0530

    Q02 - IS NULL condition

---
+++ Q02.sql
@@ -0,0 +1,8 @@
+-- Q2: Show first name and last name of patients
+--     who do not have allergies (null)
+
+SELECT first_name, last_name
+FROM patients
+WHERE allergies IS NULL;
+
+-- Result: 2059 rows returned
