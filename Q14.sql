8dd6c6277b4e33c6203a119dbc8bddf49e650ade
Ambrish Pandey <ambrishpandey.work@gmail.com>
2026-03-23 03:13:15 +0530

    Delete Q14.sql

--- Q14.sql
+++
@@ -1,8 +0,0 @@
--- Q14: Based on the cities that our patients live in,
---      show unique cities that are in province_id 'NS'
-
-SELECT DISTINCT(city) AS unique_cities
-FROM patients
-WHERE province_id = 'NS';
-
--- Result: 3 unique cities (Port Hawkesbury, Halifax, Inverness)
